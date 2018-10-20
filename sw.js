578e6ae714c1aecfc4ec9291f3a82d6d0f48059f
abugi <abugiboi@gmail.com>
2018-10-20 19:12:59 +0100

    Implemented cache only strategy

--- sw.js
+++ sw.js
@@ -10,33 +10,15 @@ TO DO:
 */
 
 //Create and add static files to cache
-const static = 'restReviewStatic-v41';
+const static = 'restReviewStatic-v42';
 const dynamic = 'restReviewDynamic-v3';
 const appShell = [
     '/',
-    '/index.html',
-    '/fallback.html',
-    '/restaurant.html',
+    '/index.html','/fallback.html','/restaurant.html',
     '/manifest.json',
-    '/css/mobile.css',
-    '/css/phablet.css',
-    '/css/styles.css',
-    '/css/tablet.css',
-    '/js/idb.js',
-    '/js/indexDB.js',
-    '/js/zdbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
+    '/css/mobile.css','/css/phablet.css','/css/styles.css','/css/tablet.css',
+    '/js/idb.js','/js/indexDB.js','/js/zdbhelper.js','/js/main.js','/js/restaurant_info.js',
+    '/img/1.jpg','/img/2.jpg','/img/3.jpg','/img/4.jpg','/img/5.jpg','/img/6.jpg','/img/7.jpg','/img/8.jpg','/img/9.jpg','/img/10.jpg',
     '/img/icons/app-icon-48x48.png',
     '/img/icons/app-icon-96x96.png',
     '/img/icons/app-icon-144x144.png',
